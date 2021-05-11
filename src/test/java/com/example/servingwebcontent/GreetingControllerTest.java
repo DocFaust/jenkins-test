57f6bf702531cbb7e788dc6c07c82e1206b6d7f8
Werner Faust <docfaust75@gmail.com>
2021-05-11 13:56:39 +0200

    Test

---
+++ src/test/java/com/example/servingwebcontent/GreetingControllerTest.java
@@ -0,0 +1,30 @@
+package com.example.servingwebcontent;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.ui.ConcurrentModel;
+import org.springframework.ui.Model;
+
+@SpringBootTest
+class GreetingControllerTest {
+
+	@Autowired
+	private GreetingController controller;
+	
+	@Test
+	public void contextLoads() {
+		assertThat(controller).isNotNull();
+	}
+	
+	@Test
+	void testgreeting() {
+		Model model = new ConcurrentModel();
+		assertThat(controller.greeting("Werner", model)).isNotNull().isEqualTo("greeting");
+		assertThat(model.getAttribute("name")).isNotNull().isEqualTo("Werner");
+		
+	}
+
+}
