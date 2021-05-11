78067bf323819e50af29292d22ec4818af15d8a4
Werner Faust <docfaust75@gmail.com>
2021-05-11 17:07:31 +0200

    private const

--- src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
+++ src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ServingWebContentApplication {
 
+	private ServingWebContentApplication() {
+		//Checkstyle wills so
+	}
+	
 	public static void main(final String[] args) {
 		SpringApplication.run(ServingWebContentApplication.class, args);
 	}
